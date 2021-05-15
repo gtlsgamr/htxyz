4df3f598915d59d1bfca2437471d0b7dbda30ed2
GtlsGamr <hittarth91@gmail.com>
2021-05-15 11:52:49 +0530

    a

--- static/js/comments-data.js
+++ static/js/comments-data.js
@@ -1,1 +1,3 @@
-var commentsobj = [{"postId":"https://www.hitarththummar.xyz/posts/mangoes_in_cardboard_boxes/","time":"Wed May 12 2021 23:43:14 GMT+0530 (India Standard Time)","alias":"Gtlsgamr","body":" First comment on this site!"},]
+var commentsobj = [{"postId":"https://www.hitarththummar.xyz/posts/mangoes_in_cardboard_boxes/","time":"Wed May 12 2021 23:43:14 GMT+0530 (India Standard Time)","alias":"Gtlsgamr","body":" First comment on this site!"},
+{"postId":"https://www.hitarththummar.xyz/posts/mahabharat_episode_5/","time":"Sat May 15 2021 11:50:59 GMT 0530 (India Standard Time)","alias":"Alex","body":"This is a nice article hittarth,\n# This engine also supports markdown. "},
+]
