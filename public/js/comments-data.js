0c8ed5fa6bef99cefd0db7109f3336350e089684
GtlsGamr <hittarth91@gmail.com>
2021-07-21 16:52:36 +0530

    changed theme.. again.

---
+++ public/js/comments-data.js
@@ -0,0 +1,5 @@
+var commentsobj = [{"postId":"https://www.hitarththummar.xyz/posts/mangoes_in_cardboard_boxes/","time":"Wed May 12 2021 23:43:14 GMT+0530 (India Standard Time)","alias":"Gtlsgamr","body":" First comment on this site!"},
+{"postId":"https://www.hitarththummar.xyz/posts/mahabharat_episode_5/","time":"Sat May 15 2021 11:50:59 GMT 0530 (India Standard Time)","alias":"Alex","body":"This is a nice article hittarth,\n# This engine also supports markdown. "},
+
+{"postId":"https://www.hitarththummar.xyz/posts/touchpadrestart/","time":"Sun May 16 2021 15:40:57 GMT+0530 (India Standard Time)","alias":"JiMMY","body":" religion ke bich me ye kaha se aa gaya.. \n\n ## ***reply from site owner*** \n\n This site is meant for all kinds of stuff that I find amusing. It is not religion specific."},
+]
--- themes/hugo-classic/archetypes/default.md
+++
@@ -1,6 +0,0 @@
----
-title: ''
-date: ''
-description: ''
-author: ''
----
