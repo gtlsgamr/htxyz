915e83e89761c44c1fcb998a4ad6701f2f5a0f16
GtlsGamr <hittarth91@gmail.com>
2021-07-22 21:26:08 +0530

    added

--- static/js/commentsdata.js
+++ static/js/commentsdata.js
@@ -2,4 +2,5 @@ var commentsdata = [
 {"postId":"https://www.hitarththummar.xyz/projects/commenting_engine/","time":"Thu Jul 22 2021 18:15:52 GMT+0530 (India Standard Time)","alias":"Hitarth Thummar","body":"#Test Comment\n## This is a test comment\n### Ok, have fun. "},
 	{"postId":"https://www.hitarththummar.xyz/posts/commenting_engine/","time":"Thu Jul 22 2021 18:33:29 GMT+0530 (India Standard Time)","alias":"Hitarth Thummar","body":"i still use it btw"},
 	{"postId":"https://www.hitarththummar.xyz/posts/commenting_engine/","time":"Thu Jul 22 2021 18:33:29 GMT+0530 (India Standard Time)","alias":"Hitarth Thummar","body":"i still use it btw, part2"},
+{"postId":"http://hitarththummar.xyz/posts/commenting_engine/","time":"Thu Jul 22 2021 21:25:40 GMT+0530 (India Standard Time)","alias":"Test Alias","body":"Test Test "},
 ];
