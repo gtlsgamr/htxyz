748bf488a57ae5c52c488592baf74b8764127ab7
GtlsGamr <hittarth91@gmail.com>
2021-07-22 18:16:37 +0530

    a

--- static/js/commentsdata.js
+++ static/js/commentsdata.js
@@ -1,3 +1,4 @@
 var commentsdata = [
 {"postId":"http://localhost:1313/projects/uldl_file_sharing_service/","time":"Thu Jul 22 2021 16:07:03 GMT 0530 (India Standard Time)","alias":"Test Alias","body":"Test Alias 1994 "},
+{"postId":"https://www.hitarththummar.xyz/projects/commenting_engine/","time":"Thu Jul 22 2021 18:15:52 GMT+0530 (India Standard Time)","alias":"Hitarth Thummar","body":"#Test Comment\n## This is a test comment\n### Ok, have fun. "},
 ];
